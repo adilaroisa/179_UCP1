da8c82e162f66c18247bc4dac2791375dc13af25
adilaroisa <adilaroisa688@gmail.com>
2025-11-04 14:48:26 +0700

    membuat kandang.js dan index.js

---
+++ models/kandang.js
@@ -0,0 +1,34 @@
+module.exports  = (sequelize, DataTypes) => {
+    const Kandang = sequelize.define('Kandang', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nama_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        nama_petugas: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        usia_hewan: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        jenis_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        tahun_lahir: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+        }
+    }, {
+        tableName: 'kandang',
+        timestamps: true,
+        freezeTableName: true
+    });
+    return Kandang;
+}
